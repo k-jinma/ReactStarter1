57de7c9bb940bcabc73c393e849f79269897bd12
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-01-18 00:00:23 +0900

    テストコンポーネントに名前とリンクを追加しました。

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -4,9 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { Test } from './Test.jsx'
 
+const name = "テスト";
+const dest = "https://yahoo.co.jp/";
+
+/* コメント */
+// コメント
 createRoot(document.getElementById('root')).render(
+  // コメント
+  /* コメント */
   <StrictMode>
     <App />
     <Test />
+    <p>Hello {name}</p>
+
+    {/* 属性値にも使用できる */}
+    <a href={dest}>Link</a>
   </StrictMode>,
 )
