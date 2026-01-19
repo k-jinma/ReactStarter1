62c6443b0dd0eb679f1e1908b68ed2e1765c0dd4
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-01-19 18:31:17 +0900

    if文を削除し、LayoutPracticeコンポーネントを直接返すように変更しました。

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,9 +1,9 @@
 import LayoutPractice from "./pages/LayoutPractice.jsx";
 
 export default function App() {
-  const mode = "practice"; // "normal" に戻すと元の表示に戻る
 
-  if (mode === "practice") {
-    return <LayoutPractice />;
-  }
+    return(
+        <LayoutPractice />
+    );
+  
 }
