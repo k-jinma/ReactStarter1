867978d75cae3cdc33aefd83c3c3302888dfe144
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-01-19 18:29:54 +0900

    if文のスタイルを改善し、可読性を向上させました。

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -3,11 +3,7 @@ import LayoutPractice from "./pages/LayoutPractice.jsx";
 export default function App() {
   const mode = "practice"; // "normal" に戻すと元の表示に戻る
 
-  if (mode === "practice") return <LayoutPractice />;
-
-  return (
-    <div>
-      {/* ここに元のAppの中身 */}
-    </div>
-  );
+  if (mode === "practice") {
+    return <LayoutPractice />;
+  }
 }
