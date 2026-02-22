cd97fa2d6afb18c87797f37d3b8107b7f08e2617
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-02-22 18:01:45 +0900

    フォームに名前フィールドのバリデーションを追加し、エラーメッセージを表示するように修正

--- react05/src/Form1.jsx
+++ react05/src/Form1.jsx
@@ -1,15 +1,25 @@
 import { useForm } from "react-hook-form"
 
 export default function Form1(){
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
 
     const onSubmit = (data) => {
         console.log(data)
+
     }
 
     return (
+        
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("firstName")} placeholder="名前"/>
+            {errors.name && <p style={{color: 'red'}}>{errors.name.message}</p>}
+            <label htmlFor="name">名前</label>
+            <input type="text" {...register("name",{
+                required:"名前は必須項目です",
+                minLength : {
+                    value: 3,
+                    message: "名前は3文字以上で入力してください"
+                }
+                })} placeholder="名前"/>
             <button type="submit">送信</button>
         </form>
     )
