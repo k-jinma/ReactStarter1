8ea83e8261da5afca524c455f171ccf2a3d414cc
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2026-04-11 10:13:54 +0900

    feat: React Routerを追加し、ルーティング機能を実装 AppコンポーネントにBrowserRouterとRoutesを追加し、映画検索アプリのページ遷移を実現

--- react10-movie/src/App.jsx
+++ react10-movie/src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import MovieDetail from './pages/MovieDetail';
 import './App.css'
 
-function App() {
 
+function App() {
   return (
-    <div className="App">
-      <h1>映画検索アプリ</h1>
-      <SearchPage />
-      <MovieDetail />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <h1>映画検索アプリ</h1>
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/movie/:movieId" element={<MovieDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
+
 
 export default App
